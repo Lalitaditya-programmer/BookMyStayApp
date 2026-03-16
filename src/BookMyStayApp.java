7becec90d79aee6708533c3463880451cd034be2
Lalitaditya Tickoo <Lalitaditya011@gmail.com>
2026-03-16 08:41:41 +0530

    Initial project setup - BookMyStayApp

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,24 @@
+/**
+ * ======================================================
+ * MAIN CLASS – BookMyStayApp
+ * ======================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
+}
